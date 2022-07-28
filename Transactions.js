497cf6ad200f8f5811539c3250ab5051c09fe6b4
Robi <robi.maliqi@hotmail.co.uk>
2022-07-28 17:49:32 +0100

    created another test which pushes each transaction in the array as an object, updated the code in the class and added a date to be pushed in the object too

--- Transactions.js
+++ Transactions.js
@@ -2,14 +2,25 @@ class Transactions {
   constructor () {
     this.balance = 0;
     this.history = [];
+    this.date = new Date().toLocaleDateString();
   }
 
   credit(amount) {
     this.balance -= amount
+    this.history.push({
+      date: this.date,
+      credit: amount,
+      balance: this.balance
+    })
   }
 
   debit(amount) {
     this.balance += amount
+    this.history.push({
+      date: this.date,
+      debit: amount,
+      balance: this.balance
+    })
   }
 
   showHistory() {
--- Transactions.test.js
+++ Transactions.test.js
@@ -8,13 +8,6 @@ describe("transactions", () => {
     })
   })
 
-  describe("showHistory", () => {
-    it('returns an empty array of transaction history', () => {
-      const transaction = new Transactions();
-      expect(transaction.showHistory()).toEqual([])
-    })
-  })
-
   describe("debit", () => {
     it('returns a new balance when the user deposits money using the debit function', () => {
       const transaction = new Transactions();
@@ -29,6 +22,22 @@ describe("transactions", () => {
       transaction.debit(2000)
       transaction.credit(1000)
       expect(transaction.showBalance()).toEqual(1000)
+    })
+  })
+
+  describe("showHistory", () => {
+    it('returns an empty array of transaction history', () => {
+      const transaction = new Transactions();
+      expect(transaction.showHistory()).toEqual([])
+    })
+
+    it('returns an array of a credit transaction and a debit transaction', () => {
+      const transaction = new Transactions();
+      transaction.debit(2000)
+      transaction.credit(1000)
+      expect(transaction.showHistory())
+        .toEqual([{"date": "28/07/2022", "debit": 2000, "balance": 2000}, 
+                  {"date": "28/07/2022", "credit": 1000, "balance": 1000}])
     })
   })
 })
